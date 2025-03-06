0d66ff4b1e1a4109885f92fe05c8fea3262a2324
rznetwork-imy <bimam186@gmail.com>
2025-03-07 00:53:08 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    // Konfigurasi untuk setiap server
+    servers: {
+        // Server 1
+        server1: {
+            name: "JATISERANA NET",
+            botToken: "7553190832:AAH9aPyOUvUMnBBzkuG-Ck7tm881eLx6GE8",
+            adminIds: ["7201805861"],
+            customers: {},
+            genieacs: {
+                baseUrl: "http://192.168.90.254:7557",
+                username: "admin",
+                password: "admin"
+            }
+        },
+        // Tambahkan server lain sesuai kebutuhan
+    }
+}; 
+
